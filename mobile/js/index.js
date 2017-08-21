455e762e781a3cf148ea740076f072efd0a06d1d
harry827 <langzhi@jiankongbao.com>
2017-08-22 00:04:42 +0800

    mobile is done

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,36 +1,53 @@
-$(function(){
-    var url ='http://ticket.e-to-china.com/';
+$(function () {
+    var url = 'http://ticket.e-to-china.com/';
 
     $('.btn_close').on('click', function () {
         $('.modal').hide();
     });
-    $('#vilate').on('click',function () {
+
+    $('#vilate').on('click', function () {
         var phone = $('#phone').val();
         $.ajax({
-            url:url+'Index/index/get_code/',
-            type:'get',
-            data:phone,
-            success:function (data) {
-                $('#msg').text(data.msg);
+            url: url + 'Index/index/get_code/',
+            type: 'get',
+            dataType: 'jsonp',
+            data: {
+                phone: phone
+            },
+            success: function (data) {
+                if (data.code != 1) {
+                    $('#msg').text(data.msg);
+                    $('.modal').show();
+                }
             }
-        }).then(function () {
-            $('.modal').show();
         });
 
     })
-    $('[type="submit"]').on('click',function(){
-        var phone=$('#phone').val();
+    $('[type="submit"]').on('click', function () {
+        var phone = $('#phone').val();
         var name = $('#name').val();
+        var code = $('#code').val();
         $.ajax({
-            url:url+'/Index/index/register/',
-            data:'phone='+phone+'&&'+'name='+name,
-            success:function (data) {
-                
+            type: 'get',
+            url: url + '/Index/index/register/',
+            dataType: 'jsonp',
+            data: {
+                phone: phone,
+                name: name,
+                code: code
             },
-            error:function (data) {
+            success: function (data) {
+                if (data.code == 1) {
+                    window.location.href = 'qcode.html?qcode=' + encodeURIComponent(data.result.ticket)
+                } else {
+                    $('#msg').text(data.msg);
+                    $('.modal').show();
+                }
 
+            },
+            error: function (data) {
+                console.log('data')
             }
         });
-        $('.modal').show();
     });
 });
